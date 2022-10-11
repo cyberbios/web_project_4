8e647088e4eda97114870b0d5c028401130e4bfc
LevMeir <alexandreisabella@hotmail.com>
2022-10-11 17:46:41 +0300

    branch futures project 4-1

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,48 @@ const jobInput = document.querySelector(".popup__input_type_subtitle");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
+const addCard = document.querySelector(".popup_type_add-card");
+const addCardFormElement = addCard.querySelector(".form");
+
+const addCardButton = document.querySelector(".profile__button");
+const addCardCloseButton = addCard.querySelector(".popup__close");
+
+const titleAddCardInput = document.querySelector(".form__input_content_title");
+const linkAddCardInput = document.querySelector(".form__input_content_link");
+
+const cardView = document.querySelector(".popup_type_card");
+
+const cards = document.querySelector(".cards");
+
+const cardTemplate = document.querySelector("#card").content;
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
