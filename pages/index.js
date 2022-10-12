9505cca6b3c34c7c5aa6ac945b0ce599793d755b
LevMeir <alexandreisabella@hotmail.com>
2022-10-12 15:31:06 +0300

    cards, trash button, like button, change pictures

--- pages/index.js
+++ pages/index.js
@@ -36,19 +36,19 @@ const cardTemplate = document.querySelector("#card").content;
 const initialCards = [
   {
     name: "Cousteau’s underwater classroom: Imparting lessons from under the sea.",
-    link: "../images/cousteau_underwater.png",
+    link: "https://ogden_images.s3.amazonaws.com/www.mauinews.com/images/2020/02/12060413/CEV3927-1-cousteau.jpg",
   },
   {
     name: "Jacques Cousteau Wearing Diving Gear by Bettmann",
-    link: "../images/jacques-cousteau-wearing-diving-gear-bettmann.png",
+    link: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/jacques-cousteau-wearing-diving-gear-bettmann.jpg",
   },
   {
     name: "Watercraft of Explorer, Invented by Jacques Cousteau",
-    link: "../images/watercraft.png",
+    link: "http://35410006.weebly.com/uploads/7/1/8/3/71834201/5553598_orig.jpg",
   },
   {
     name: "Jacques Cousteau, Calypso boat and the Watercrafts, Diving around the World",
-    link: "../images/calypso.png",
+    link: "https://i.pinimg.com/originals/7d/b4/de/7db4de0c201fc36c8da989de15717bd5.jpg",
   },
   {
     name: "Jacques Cousteau, underwater man",
